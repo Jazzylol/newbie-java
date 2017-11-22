f04d8e1fe3a3c8053fd64e20fd835f27ce66d1fe
Jazz <506496088@qq.com>
2017-11-23 00:30:00 +0800

    添加解答

---
+++ src/main/java/com/coldridge/valley/java/grammar/object/Learn10.java
@@ -0,0 +1,8 @@
+package com.coldridge.valley.java.grammar.object;
+
+
+/**
+ * this关键字用法，代表的含义
+ */
+public class Learn10 {
+}
---
+++ src/main/java/com/coldridge/valley/java/grammar/object/Learn9.java
@@ -0,0 +1,17 @@
+package com.coldridge.valley.java.grammar.object;
+
+
+/**
+ * 封装的好处是什么
+ */
+public class Learn9 {
+
+
+    /***
+     * https://www.zhihu.com/question/65423765
+     */
+
+
+
+
+}
